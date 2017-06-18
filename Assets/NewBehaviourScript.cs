163c249bdb84fc712d25a00e17c87ac5216a46c7
Novack <>
2017-06-18 00:12:06 -0300

    Initial Commit.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu()]
+public class NewBehaviourScript : ScriptableObject
+{
+	[Serializable]
+	public class marisco
+	{
+		public string name;
+		public int size;
+	}
+	
+	[HideInInspector]
+	public int none;
+	public float lala;
+	public List<int> lele;
+	public int[] lilo;
+	public string strele;
+	[Header("gffgsfdcg")]
+	public Color col;
+	[Space(10)]
+	public Vector3 vec;
+	[Tooltip("vervever")]
+	public marisco caracok;
+	
+
+}
