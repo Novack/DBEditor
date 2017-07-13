015d04177221b08cd69c99341e8f3e180a4d1520
Novack <>
2017-07-13 10:20:57 -0300

    Working key field.

--- Assets/DBEditor/Editor/PropertyDrawers/LocalizationDrawer.cs
+++ Assets/DBEditor/Editor/PropertyDrawers/LocalizationDrawer.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class LocalizationDrawer : PropertyDrawer
 {
     private const float TEXT_HEIGHT = 16f;
-    private const float SEPARATOR_HEIGHT = 2f;
+	private const float SEPARATOR_HEIGHT = 2f;
+	private const float ENDING_SEPARATOR_HEIGHT = 3f;
 
     private LocalizationAttribute _localizationAttribute { get { return ((LocalizationAttribute)attribute); } }
 
@@ -21,39 +22,42 @@ public class LocalizationDrawer : PropertyDrawer
 	    if (_localizationAttribute.isMultiline)
 		    height = TEXT_HEIGHT * 4;
 	    
-	    return baseHeight + height + SEPARATOR_HEIGHT;
+	    return baseHeight + height + SEPARATOR_HEIGHT + ENDING_SEPARATOR_HEIGHT;
     }
 
     // Here you can define the GUI for your property drawer. Called by Unity.
     public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
     {
+	    _locKeyValue = prop.stringValue;
+	    _translatedValue = GetTranslation(_locKeyValue);
+	    
 	    DrawTextField(position, prop, label);
         DrawLocalizedField(position, prop);
     }
-        
+
     void DrawTextField(Rect position, SerializedProperty prop, GUIContent label)
     {
         Rect textFieldPosition = position;
         textFieldPosition.height = TEXT_HEIGHT;
-
-        _locKeyValue = prop.stringValue;
-
+	    
+	    EditorGUI.BeginChangeCheck();
         GUI.SetNextControlName("user");
-        if (EditorGUI.TextField(textFieldPosition, label, _locKeyValue).KeyPressed("user", KeyCode.Return, out _locKeyValue))
-        {
-            prop.stringValue = _locKeyValue;
+	    if (EditorGUI.TextField(textFieldPosition, label, _locKeyValue).KeyPressed("user", KeyCode.Return, out _locKeyValue))
+	    {
             _translatedValue = GetTranslation(_locKeyValue);
-        }
+	    }
+	    
+	    if (EditorGUI.EndChangeCheck())
+		    prop.stringValue = _locKeyValue;
     }
 
     void DrawLocalizedField(Rect position, SerializedProperty prop)
     {
-        _translatedValue = GetTranslation(_locKeyValue);
-
         Rect localizedPosition = EditorGUI.IndentedRect(position);  
 	    localizedPosition.y = localizedPosition.y + TEXT_HEIGHT;
 	    localizedPosition.height = TEXT_HEIGHT;
-
+	    
+	    EditorGUI.BeginChangeCheck();
         if (_localizationAttribute.isMultiline)
         {
         	EditorGUI.LabelField(localizedPosition, "Localized:");	        	
@@ -65,11 +69,21 @@ public class LocalizationDrawer : PropertyDrawer
 	    {
 	        localizedPosition.y = localizedPosition.y + SEPARATOR_HEIGHT;
             _translatedValue = EditorGUI.TextField(localizedPosition, "Localized:", _translatedValue);
-        }
+	    }
+	    
+	    if (EditorGUI.EndChangeCheck())
+		    Debug.Log(_translatedValue);
     }
 
     private string GetTranslation(string locKey)
     {
-        return Random.Range(1, 1000).ToString() + locKey;
+	    string[] localizationDemo = new string[]{"pepe", "juan", "largo\nmultilinea", "algo mas"};
+	    
+	    int key;
+	    bool keyParsed = int.TryParse(locKey, out key);
+	    if (keyParsed && key < localizationDemo.Length)
+		    return localizationDemo[key];
+	    
+	    return "Loc key not found";
     }
 }
