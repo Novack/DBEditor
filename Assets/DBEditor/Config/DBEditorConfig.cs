42eaa91bb82d6a336c414930998f38b583417b72
Novack <>
2017-08-25 17:20:46 -0300

    Fix on config.

--- Assets/DBEditor/Config/DBEditorConfig.cs
+++ Assets/DBEditor/Config/DBEditorConfig.cs
@@ -11,12 +11,12 @@ namespace DBEditor
 		[Serializable]
 		public class Config
 		{
+			[Tooltip("The parent category structure for this files. Use '/' to nest.")]
+			public string TreeViewPath;
 			[Tooltip("The path to the folder to contain newly created objects of this category. 'Assets/...'")]
 			public string FileStoragePath = "Assets/...";
 			[Tooltip("The ScriptableObject script class name.")]
 			public string ClassName;
-			[Tooltip("The parent category structure for this files. Use '/' to nest.")]
-			public string TreeViewPath;
 			[Tooltip("The ScriptableObject instances to include manually.")]
 			public List<ScriptableObject> Files;
 		}
